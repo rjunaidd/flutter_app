1246e5ff546f4dd3fd04296cc8c5f1cdcd50857a
Rauf Junaid <roafjunaid09@gmail.com>
2026-05-12 23:50:22 +0500

    update code

--- lib/currency_converter.dart
+++ lib/currency_converter.dart
@@ -13,24 +13,59 @@ class CurrencyConverter extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            Text(
-              "Hello World",
-              style: TextStyle(
-                color: Colors.indigo.shade800,
-                fontSize: 40,
-                fontWeight: FontWeight.w800,
+            Container(
+              padding: EdgeInsets.all(20),
+              margin: EdgeInsets.all(20),
+              color: Colors.white24,
+              height: 100,
+              width: 200,
+              child: Text(
+                "Hello World",
+                style: TextStyle(
+                  color: Colors.indigo.shade800,
+                  fontSize: 40,
+                  fontWeight: FontWeight.w800,
+                ),
               ),
             ),
-            TextField(
-              style: TextStyle(color: Colors.white),
-              decoration: InputDecoration(
-                hintText: 'Please! Enter enter the amount in USD',
-                hintStyle: TextStyle(color: Colors.white60),
-                prefixIcon: Icon(Icons.money_off_csred_outlined),
-                prefixIconColor: Colors.black,
-                fillColor: Colors.white24
+            Padding(
+
+              padding: const EdgeInsets.symmetric(
+                horizontal: 40
+              ),
+              child: TextField(
+                style: TextStyle(color: Colors.white),
+                decoration: InputDecoration(
+                  hintText: 'Please! Enter enter the amount in USD',
+                  hintStyle: TextStyle(color: Colors.black),
+                  prefixIcon: Icon(Icons.money_off_csred_outlined),
+                  prefixIconColor: Colors.black,
+                  fillColor: Colors.white,
+                  filled: true,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      width: 2,
+                      style: BorderStyle.solid
+                    ),
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  enabledBorder:UnderlineInputBorder()
+                ),
+                keyboardType: TextInputType.number,
               ),
             ),
+            SizedBox(height: 30,),
+
+            TextButton(onPressed: (){
+              print("Button Clicked!");
+            },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.amber,
+                  foregroundColor: Colors.black,
+                  fixedSize: Size((double.infinity), 30)
+                ),
+                child: Text("Click Here")),
+
           ],
         ),
       ),
